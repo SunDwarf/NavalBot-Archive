9532942c1369e2bd5c9acfd75d3586187923d90e
SunDwarf <sun@veriny.tf>
2016-04-06 12:29:37 +0100

    Fix slice in format()

--- cmds/commits.py
+++ cmds/commits.py
@@ -78,6 +78,6 @@ async def check_for_commits(client: discord.Client):
                     # Create a message.
                     await client.send_message(
                         chan,
-                        "**{data[sha][0:10]}**: {data[commit][message]}\n".format(data=commit) +
+                        "**{sha}**: {data[commit][message]}\n".format(data=commit, sha=commit['sha'][0:10]) +
                         "by *{data[commit][committer][name]".format(data=commit)
                     )
