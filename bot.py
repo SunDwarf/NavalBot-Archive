31a9ab32ca379c1c1c5fddce689ac5d0d831008a
SunDwarf <sun@veriny.tf>
2016-04-11 16:37:44 +0100

    2.4.0
    Signed-off-by: SunDwarf <sun@veriny.tf>

--- bot.py
+++ bot.py
@@ -121,7 +121,7 @@ CREATE TABLE IF NOT EXISTS configuration (
 """)
 
 # Version information.
-VERSION = "2.3.6"
+VERSION = "2.4.0"
 VERSIONT = tuple(int(i) for i in VERSION.split("."))
 
 # Factoid matcher compiled
