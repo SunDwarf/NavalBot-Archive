ae161f08c6c00b7bc34290213c169f8aa3f20a1b
ilevn <nilsntth@gmail.com>
2016-04-25 19:47:05 +0200

    add factoid command, closes #10

--- cmds/fun.py
+++ cmds/fun.py
@@ -278,3 +278,13 @@ async def echo(client: discord.Client, message: discord.Message, args: list):
         return
 
     await client.send_message(channel, args[2])
+
+
+@cmds.command("factoid")
+async def factoid(client: discord.Client, message: discord.Message):
+    """
+    Displays help on how to create a factoid.
+    """
+    prefix = util.get_config(message.server.id, "command_prefix", "?")
+    await client.send_message(message.server,
+                              "You can create a factoid by typing `{}<factoid_name> is <answer>`".format(prefix))
