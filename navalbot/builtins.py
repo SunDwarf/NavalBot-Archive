753c8417b1540b5c6ef2d3f696c2f0d742e156b3
SunDwarf <sun@veriny.tf>
2016-05-22 13:17:21 +0100

    Better factoids - inline commands are now much more powerful.

--- navalbot/builtins.py
+++ navalbot/builtins.py
@@ -29,6 +29,7 @@ import re
 
 from navalbot.api.commands import commands, command, Command, CommandContext
 from navalbot.api import db
+from navalbot.api.locale import get_locale
 from navalbot.api.util import sanitize, get_file
 
 from navalbot import factoids
@@ -65,67 +66,70 @@ async def default(client: discord.Client, message: discord.Message):
     Delegates to factoids.delegate().
     """
     # Create a new context.
-    ctx = CommandContext()
-    await factoids.delegate(client, message)
+    loc = await db.get_config(message.server.id, "lang", default=None)
+    loc = get_locale(loc)
+
+    ctx = CommandContext(client, message, loc)
+    # Delegate factoids to handler to handle it.
+    await factoids.delegate(ctx)
 
 
-async def default(client: discord.Client, message: discord.Message):
-    prefix = await db.get_config(message.server.id, "command_prefix", "?")
-    data = message.content[len(prefix):]
-    # Check if it matches a factoid creation
-    matches = factoid_matcher.match(data)
-    if matches:
-        # Set the factoid
-        name = matches.groups()[0]
-        fac = matches.groups()[1]
-        if not (len(name) > 0 and len(fac) > 0):
-            return
-        # Check if it's locked.
-        locked = await db.get_config(message.server.id, "fac:{}:locked".format(name), default=None)
-        if locked and locked != message.author.id:
-            await client.send_message(message.channel, ":x: Cannot edit, factoid is locked to ID `{}`.".format(locked))
-            return
-        assert isinstance(fac, str)
-        if fac.startswith("http") and 'youtube' not in fac:
-            # download as a file
-            file = sanitize(fac.split('/')[-1])
-            client.loop.create_task(get_file((client, message), url=fac, name=file))
-            fac = "file:{}".format(file)
-        await db.set_config(message.server.id, "fac:{}".format(name), fac)
-        await client.send_message(message.channel, ":heavy_check_mark: Factoid `{}` is now `{}`".format(name, fac))
-    else:
-        # Load content
-        content = await db.get_config(message.server.id, "fac:{}".format(data))
-        if not content:
-            return
-        # Check if it's an inline command.
-        inline_cmd = command_matcher.match(content)
-        if inline_cmd:
-            # Run the inline command.
-            sp = inline_cmd.groups()[0]
-            first_word = sp.split(" ")[0]
-            prefix = await db.get_config(message.server.id, "command_prefix", "?")
-            if first_word.startswith(prefix):
-                first_word = first_word[len(prefix):]
-            # load it from commands
-            print(first_word, commands)
-            if first_word not in commands:
-                await client.send_message(message.channel, ":x: Inline command `{}` does not exist".format(first_word))
-                return
-            # Get it, and invoke.
-            message.content = sp
-            command = commands[first_word]
-            await command.invoke(client, message)
-            return
-
+#async def default(client: discord.Client, message: discord.Message):
+#    prefix = await db.get_config(message.server.id, "command_prefix", "?")
+#    data = message.content[len(prefix):]
+#    # Check if it matches a factoid creation
+#    matches = factoid_matcher.match(data)
+#    if matches:
+#        # Set the factoid
+#        name = matches.groups()[0]
+#        fac = matches.groups()[1]
+#        if not (len(name) > 0 and len(fac) > 0):
+#            return
+#        # Check if it's locked.
+#        locked = await db.get_config(message.server.id, "fac:{}:locked".format(name), default=None)
+#        if locked and locked != message.author.id:
+#            await client.send_message(message.channel, ":x: Cannot edit, factoid is locked to ID `{}`.".format(locked))
+#            return
+#        assert isinstance(fac, str)
+#        if fac.startswith("http") and 'youtube' not in fac:
+#            # download as a file
+#            file = sanitize(fac.split('/')[-1])
+#            client.loop.create_task(get_file((client, message), url=fac, name=file))
+#            fac = "file:{}".format(file)
+#        await db.set_config(message.server.id, "fac:{}".format(name), fac)
+#        await client.send_message(message.channel, ":heavy_check_mark: Factoid `{}` is now `{}`".format(name, fac))
+#    else:
+#        # Load content
+#        content = await db.get_config(message.server.id, "fac:{}".format(data))
+#        if not content:
+#            return
+#        # Check if it's an inline command.
+#        inline_cmd = command_matcher.match(content)
+#        if inline_cmd:
+#            # Run the inline command.
+#            sp = inline_cmd.groups()[0]
+#            first_word = sp.split(" ")[0]
+#            prefix = await db.get_config(message.server.id, "command_prefix", "?")
+#            if first_word.startswith(prefix):
+#                first_word = first_word[len(prefix):]
+#            # load it from commands
+#            if first_word not in commands:
+#                await client.send_message(message.channel, ":x: Inline command `{}` does not exist".format(first_word))
+#                return
+#            # Get it, and invoke.
+#            message.content = sp
+#            command = commands[first_word]
+#            await command.invoke(client, message)
+#            return
+#
         # Check if it's a file
-        if content.startswith("file:"):
-            fname = content.split("file:")[1]
-            if not os.path.exists(os.path.join(os.getcwd(), 'files', fname)):
-                await client.send_message(message.channel, ":x: Unknown error: File {} does not exist".format(fname))
-                return
-            # Load the file
-            with open(os.path.join(os.getcwd(), 'files', fname), 'rb') as f:
-                await client.send_file(message.channel, f)
-            return
-        await client.send_message(message.channel, content)
+#        if content.startswith("file:"):
+#            fname = content.split("file:")[1]
+#            if not os.path.exists(os.path.join(os.getcwd(), 'files', fname)):
+#                await client.send_message(message.channel, ":x: Unknown error: File {} does not exist".format(fname))
+ ##               return
+  ##          # Load the file
+   ##         with open(os.path.join(os.getcwd(), 'files', fname), 'rb') as f:
+    #            await client.send_file(message.channel, f)
+    #        return
+    #    await client.send_message(message.channel, content)
--- navalbot/factoids.py
+++ navalbot/factoids.py
@@ -22,4 +22,104 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 =================================
 """
+import re
+import shlex
 
+from navalbot.api.commands import CommandContext, commands
+
+# Factoid matcher compiled
+from navalbot.api.util import sanitize, get_file
+
+factoid_matcher = re.compile(r'(\S*?) is (.*)')
+
+# Command matcher compiled
+command_matcher = re.compile(r'{(.*)}')
+
+
+async def delegate(ctx: CommandContext):
+    """
+    Factoid delegate handler.
+    """
+    prefix = await ctx.get_config("command_prefix", "?")
+    data = ctx.message.content[len(prefix):]
+    # Match it.
+    matches = factoid_matcher.match(data)
+    if matches:
+        await set_factoid(ctx, matches)
+    else:
+        await get_factoid(ctx, data)
+
+
+async def set_factoid(ctx: CommandContext, match):
+    """
+    Sets a new factoid.
+
+    Parses commands as appropriate.
+    """
+    name = match.groups()[0]
+    fac = match.groups()[1]
+    if not (len(name) > 0 and len(fac) > 0):
+        return
+    # Check if it's locked.
+    locked = await ctx.get_config("factoid:{}:locked".format(name))
+    if locked and locked != ctx.author.id:
+        await ctx.reply("core.factoids.cannot_edit", fac=name, u=locked)
+        return
+
+    # Download the factoid, if applicable.
+    if fac.startswith("http") and 'youtube' not in fac:
+        # download as a file
+        file = sanitize(fac.split('/')[-1])
+        await get_file((ctx.client, ctx.message), url=fac, name=file)
+        fac = "file:{}".format(file)
+
+    await ctx.set_config("fac:{}".format(name), fac)
+    await ctx.reply("core.factoids.set", name=name, content=fac)
+
+async def get_factoid(ctx: CommandContext, data: str):
+    """
+    Loads a factoid from the DB.
+    """
+    prefix = await ctx.get_config("command_prefix", "?")
+    # Split data apart and load that factoid, because fuck spaces.
+    to_load = data.split(" ")[0]
+    content = await ctx.get_config("fac:{}".format(to_load))
+    if not content:
+        # Don't do anything.
+        return
+
+    # Check if it is an inline command.
+    inline_cmd = command_matcher.match(content)
+    if inline_cmd:
+        # It matches, so extract out everything.
+        grp = inline_cmd.groups()
+        # Load the command word, and check it.
+        data = grp[0]
+        command_word = grp[0].split(" ")[0]
+
+        if command_word.startswith(prefix):
+            command_word = command_word[len(prefix):]
+
+        # Check if it is in commands.
+        if command_word not in commands:
+            await ctx.reply("generic.cannot_find_command".format(command_word))
+            return
+
+        # Load out the command.
+
+        # Load up the old arguments and content
+        old_content = ctx.message.content
+        try:
+            old_args = shlex.split(old_content)[1:]
+        except ValueError:
+            old_args = old_content.split(" ")[1:]
+
+        # Format the new content, using the inline command args.
+        message = ctx.message
+        client = ctx.client
+        print(data)
+        ctx.message.content = data.format(*old_args, full=' '.join(old_args))
+
+        # Invoke the new function.
+        command = commands[command_word]
+        await command.invoke(client, message)
