35f614b54e7c551185c1a42727526c786e8d3c31
SunDwarf <sun@veriny.tf>
2016-06-01 16:37:18 +0100

    -rc1

--- navalbot/version.py
+++ navalbot/version.py
@@ -19,5 +19,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 """
 
 VERSION = "6.0.0"
-VERSUFF = "-beta2"
+VERSUFF = "-rc1"
 VERSIONT = tuple(int(i) for i in VERSION.split("."))
